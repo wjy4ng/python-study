392180a88e553c5fbba44de294a116f9b45febfc
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-06-20 20:47:52 +0900

    feat: django 회원가입 구현

--- Django/proj01/users/views.py
+++ Django/proj01/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from .models import User
 
 # Create your views here.
 def login_view(request):
@@ -20,3 +21,23 @@ def logout_view(request):
     logout(request)
 
     return redirect("user:login")
+
+def signup_view(request):
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST["username"]
+        password = request.POST["password"]
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        email = request.POST["email"]
+        student_id = request.POST["student_id"]
+
+        user = User.objects.create_user(username, email, password)
+        user.last_name = lastname
+        user.first_name = firstname
+        user.student_id = student_id
+        user.save()
+
+        return redirect("user:login")
+
+    return render(request, 'users/signup.html')

b5573c22708a0bf51545b19915ac9651691a7a75
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-06-20 19:47:18 +0900

    feat: django 로그인 구현

--- Django/proj01/proj01/urls.py
+++ Django/proj01/proj01/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
-    path('login/', include('users.urls')),
+    path('auth/', include('users.urls')),
 ]
--- Django/proj01/users/views.py
+++ Django/proj01/users/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            print("로그인 성공")
+            login(request, user)
+        else:
+            print("로그인 실패")
+
     return render(request, 'users/login.html')

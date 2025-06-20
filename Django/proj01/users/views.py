1781bb33af7641969903175d6d97038d4279a615
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-06-20 20:21:47 +0900

    feat: django 로그아웃 구현

--- Django/proj01/users/views.py
+++ Django/proj01/users/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
-def login(request):
+def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -15,3 +15,8 @@ def login(request):
             print("로그인 실패")
 
     return render(request, 'users/login.html')
+
+def logout_view(request):
+    logout(request)
+
+    return redirect("user:login")

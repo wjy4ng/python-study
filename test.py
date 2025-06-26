ef618b2854b503af1df918b28e01dc136966decc
wjyang <83898622+wjy4ng@users.noreply.github.com>
2025-06-26 11:47:09 +0900

    feat: 커밋 충돌 테스트를 위한 코드 삽입

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ class App():
     def __init__(self):
         self.name = "양원준"
         self.age = 24
+        self.job = "student_in_KNU"
     
     def print_test(self):
         print("test입니다")

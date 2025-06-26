3ec17ecb5ade0df9edd8ce96e9c145b2a9e2fa90
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-06-26 12:22:17 +0900

    fix: 깃 충돌 테스트 코드 수정

--- test.py
+++ test.py
@@ -1,18 +1,1 @@
-class App():
-    test_id = 1
-
-    def __init__(self):
-        self.id = App.test_id
-        App.test_id += 1
-    
-    def print_test(self):
-        print(f"test id : {self.id}")
-
-
-if __name__=="__main__":
-    app1 = App()
-    app2 = App()
-
-    app1.print_test()
-    app2.print_test()
-
+print("깃 충돌 테스트")
